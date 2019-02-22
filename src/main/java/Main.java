00f3588546c448beff7933845702dc1a54dfa437
EnragedKolibri <enragedkolibri@gmail.com>
2019-02-22 19:33:43 +0200

    yay

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 import javafx.application.Application;
-
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -11,11 +10,50 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Main extends Application{
 
 
+    static int suparPiecOfShittConstant = 32;
+    static ArrayList<File> files = new ArrayList<>();
+
+    static {
+        files.add(new File("assets/1470105_3.png"));
+        files.add(new File("assets/2473250_1.png"));
+    }
+
+    private static int[][] mapa =
+            {
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+
+
+            };
+
+    private static int[][] mapa1 =
+            {
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+
+            };
 
 
     public static void main(String[] args){
@@ -25,50 +63,50 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) throws Exception{
 
-        int ultraShittedWidth = zachemYaJivy(50);
-        int ultraShittedHeight = zachemYaJivy(20);
-        int suparPiecOfShittConstant = 32;
+//        int ultraShittedWidth = zachemYaJivy(50);
+
+        int ultraShittedWidth = mapa[0].length * suparPiecOfShittConstant;
+        int ultraShittedHeight = mapa.length * suparPiecOfShittConstant;
+//        int ultraShittedHeight = zachemYaJivy(20);
+
+        System.out.println(ultraShittedWidth);
+        System.out.println(ultraShittedHeight);
+        System.out.println();
 
         Pane root = new StackPane();
         root.setPrefSize(ultraShittedWidth,ultraShittedHeight);
 
         Scene s = new Scene(root, ultraShittedWidth, ultraShittedHeight, Color.BLACK);
 
-        ArrayList<File> files = new ArrayList<>();
-        files.add(new File("assets/1470105_3.png"));
-        files.add(new File("assets/2473250_1.png"));
-
-        Random random = new Random();
 
         Canvas canvas = new Canvas(ultraShittedWidth, ultraShittedHeight);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        root.getChildren().addAll(canvas);
+        root.getChildren().add(canvas);
 
         //Layer filler algorithm proto
-        int x = 0;
-        int y = 0;
-        int counter = 0;
-        for (int i = 0; i != ultraShittedWidth; i++)
-        {
-            if (x == ultraShittedWidth)
-            {
-                y+=suparPiecOfShittConstant;
-                x=0;
-            }
-            if (random.nextInt(10)>5)
-            {
-                counter ++;
-            }else
-            {
-                if (counter>0)
-                {
-                    counter--;
-                }
+//        int x = 0;
+//        int y = 0;
+//        for (int i = 0; i != ultraShittedWidth; i++) {
+//            if (x == ultraShittedWidth) {
+//                y+=suparPiecOfShittConstant;
+//                x=0;
+//            }
+//            gc.drawImage(imageProcessor(files), x , y,suparPiecOfShittConstant,suparPiecOfShittConstant);
+//            x+=suparPiecOfShittConstant;
+//        }
 
+        for (int y = 0; y <  mapa.length; y++) {
+            int yC = suparPiecOfShittConstant * y;
+
+            System.out.println("y " + yC);
+            for (int x = 0; x < mapa[0].length ; x ++) {
+                int xC = suparPiecOfShittConstant * x;
+
+                gc.drawImage(imageProcessor(mapa[y][x]), xC , yC,suparPiecOfShittConstant,suparPiecOfShittConstant);
             }
-            gc.drawImage(imageProcessor(files,counter), x , y,suparPiecOfShittConstant,suparPiecOfShittConstant);
-            x+=suparPiecOfShittConstant;
         }
+
+
         stage.setScene(s);
         stage.show();
     }
@@ -76,16 +114,15 @@ public class Main extends Application{
 
     public int zachemYaJivy(int pzdc)
     {
-        return pzdc*32;
+        return pzdc*suparPiecOfShittConstant;
     }
 
-    public Image imageProcessor(ArrayList<File> file, int counter){
-
-        if (counter == 1)
-        {
-
-            return new Image(file.get(1).toURI().toString());
-        }
-        return new Image(file.get(0).toURI().toString());
+//    public Image imageProcessor(List<File> file){
+//
+//        return new Image(file.get(new Random().nextInt(file.size())).toURI().toString());
+//    }
+//
+    public Image imageProcessor(int i){
+        return new Image(files.get(i).toURI().toString());
     }
 }
